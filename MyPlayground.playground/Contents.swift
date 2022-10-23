55f724a01c63792bf39b8d1cec65938d43379540
Mut Tony <muty05@yahoo.com>
2022-10-23 14:54:09 +0300

    Data types in Swift
    Different data types in swift, including Int, String, Float, Double, Bool

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+/**
+ 1. Variables and Constatnts
+ 2. using "var" and "let" keywords to declare variables and constants
+ 3. using equal sign to assign data to varaibles or constants
+ 4. camelCase best practice for varaible naming
+ 5. constants are like variables but you cannot reassign data to them after initail assignment
+ **/
+
+var greeting = "Hello, playground"
+
+//Variables
+var firstname = "Andrew"
+
+print(firstname)
+
+var stockPrice = 100
+print(stockPrice)
+
+stockPrice = 50
+print(stockPrice)
+
+stockPrice = 43
+
+//conatcnts
+
+let  lastname = "Smith"
+
+//Practice 1
+var firstName = "Joseph"
+var lastName = "Smith"
+let gender = "Male"
+var age = "18"
+var cashOnHand = "100"
+
+print(firstName)
+print(lastName)
+print(gender)
+print(age)
+print(cashOnHand)
+
+/**
+ Data type
+ 
+ Int, String, Float, Double, Bool
+ */
+
+var myAge:Int = 18
+var myHeight:Float = 170.5
+var bankBalance:Double = 100000.4
+var isMarried:Bool = false
